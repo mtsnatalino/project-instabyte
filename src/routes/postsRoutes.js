9b8013da8fa9db572cdc8f9a24773b0197917f5f
mtsnatalino <matheuspedro982@gmail.com>
2024-12-01 11:24:09 -0300

    finalizacao do env

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import multer from 'multer';
-import { listarPosts, postarNovoPost, uploadImagem } from '../controller/postController.js';
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from '../controller/postController.js';
+import cors from 'cors';
+
+const corsOptions = {
+    origin: "http://localhost:8000",
+    optionsSuccessStatus: 200 
+}
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
@@ -14,13 +20,17 @@ const storage = multer.diskStorage({
 const upload = multer({dest: "./uploads", storage});
 
 const routes = (app) => {
-    app.use(express.json())
+    app.use(express.json());
+    
+    app.use(cors(corsOptions));
 
     app.get("/posts", listarPosts);
 
     app.post("/posts", postarNovoPost);
 
     app.post("/upload", upload.single("imagem"), uploadImagem);
+
+    app.put("/upload/:id", atualizarNovoPost);
 }
 
 export default routes;
